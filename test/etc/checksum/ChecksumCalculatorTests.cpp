dea5f7b047b792058e1d1e1029d4c50da01b5619
Timor Gruber <timor.gruber@gmail.com>
2019-08-14 23:54:25 +0300

    Separated test scenarios for each overload

--- test/etc/checksum/ChecksumCalculatorTests.cpp
+++ test/etc/checksum/ChecksumCalculatorTests.cpp
@@ -10,13 +10,97 @@
 
 #include <proto_nmea/etc/checksum/ChecksumCalculator.h>
 
-SCENARIO("Checksum (XOR) calculated correctly")
+SCENARIO("XOR range checksum calculation handles invalid input")
+{
+    GIVEN("Null begin string")
+    {
+        AND_GIVEN("Non-Empty end string")
+        {
+            std::string str{"abc"};
+
+            WHEN("Checksum is calculated")
+            {
+                auto result = calculateRangeChecksum(nullptr,
+                                                     reinterpret_cast<const unsigned char *>(str.c_str()));
+
+                THEN("Result is zero")
+                {
+                    REQUIRE(result == 0);
+                }
+            }
+        }
+        AND_GIVEN("Empty end string")
+        {
+            std::string str;
+
+            WHEN("Checksum is calculated")
+            {
+                auto result = calculateRangeChecksum(nullptr,
+                                                     reinterpret_cast<const unsigned char *>(str.c_str()));
+
+                THEN("Result is zero")
+                {
+                    REQUIRE(result == 0);
+                }
+            }
+        }
+    }
+
+    GIVEN("Null end string")
+    {
+        AND_GIVEN("Non-Empty begin string")
+        {
+            std::string str{"abc"};
+
+            WHEN("Checksum is calculated")
+            {
+                auto result = calculateRangeChecksum(reinterpret_cast<const unsigned char *>(str.c_str()),
+                                                     nullptr);
+
+                THEN("Result is not zero")
+                {
+                    REQUIRE(result != 0);
+                }
+            }
+        }
+        AND_GIVEN("Empty beginning string")
+        {
+            std::string str;
+
+            WHEN("Checksum is calculated")
+            {
+                auto result = calculateRangeChecksum(reinterpret_cast<const unsigned char *>(str.c_str()),
+                                                     nullptr);
+
+                THEN("Result is zero")
+                {
+                    REQUIRE(result == 0);
+                }
+            }
+        }
+    }
+
+    GIVEN("Both begin & end are null")
+    {
+        WHEN("Checksum is calculated")
+        {
+            auto result = calculateRangeChecksum(nullptr, nullptr);
+
+            THEN("Result is zero")
+            {
+                REQUIRE(result == 0);
+            }
+        }
+    }
+}
+
+SCENARIO("XOR range checksum calculated correctly")
 {
     GIVEN("Zero-length string")
     {
         std::string str;
 
-        WHEN("Range-based calculation is performed")
+        WHEN("Checksum is calculated")
         {
             auto result = calculateRangeChecksum(reinterpret_cast<const unsigned char *>(str.c_str()),
                                                  nullptr);
@@ -32,11 +116,12 @@ SCENARIO("Checksum (XOR) calculated correctly")
     {
         std::string str{"abc"};
 
-        WHEN("Range-based calculation is performed")
+        WHEN("Range consists of entire string")
         {
-            AND_WHEN("Range consists of entire string")
+            const int expectedChecksum = 0x60; // Calculated by 'ScadaCore' - 3rd-Party calculator
+
+            AND_WHEN("Checksum is calculated")
             {
-                const int expectedChecksum = 0x60; // Calculated by 'ScadaCore' - 3rd-Party calculator
                 auto result = calculateRangeChecksum(
                         reinterpret_cast<const unsigned char *>(str._Unchecked_begin()),
                         reinterpret_cast<const unsigned char *>(str._Unchecked_end()));
@@ -46,9 +131,13 @@ SCENARIO("Checksum (XOR) calculated correctly")
                     REQUIRE(result == expectedChecksum);
                 }
             }
-            AND_WHEN("Range consists of first 2 elements of the string")
+        }
+        WHEN("Range consists of first 2 elements of the string")
+        {
+            const int expectedChecksum = 0x03; // Calculated by 'ScadaCore' - 3rd-Party calculator
+
+            AND_WHEN("Checksum is calculated")
             {
-                const int expectedChecksum = 0x03; // Calculated by 'ScadaCore' - 3rd-Party calculator
                 auto result = calculateRangeChecksum(
                         reinterpret_cast<const unsigned char *>(str._Unchecked_begin()),
                         reinterpret_cast<const unsigned char *>(str._Unchecked_begin() + 2));
@@ -58,9 +147,13 @@ SCENARIO("Checksum (XOR) calculated correctly")
                     REQUIRE(result == expectedChecksum);
                 }
             }
-            AND_WHEN("Range consists of last 2 elements of the string")
+        }
+        WHEN("Range consists of last 2 elements of the string")
+        {
+            const int expectedChecksum = 0x01; // Calculated by 'ScadaCore' - 3rd-Party calculator
+
+            AND_WHEN("Checksum is calculated")
             {
-                const int expectedChecksum = 0x01; // Calculated by 'ScadaCore' - 3rd-Party calculator
                 auto result = calculateRangeChecksum(
                         reinterpret_cast<const unsigned char *>(str._Unchecked_end() - 2),
                         reinterpret_cast<const unsigned char *>(str._Unchecked_end()));
@@ -68,77 +161,6 @@ SCENARIO("Checksum (XOR) calculated correctly")
                 THEN("Result is the XOR-8 of all elements in range")
                 {
                     REQUIRE(result == expectedChecksum);
-                }
-            }
-        }
-    }
-}
-
-SCENARIO("Checksum (XOR) calculator handles invalid input")
-{
-    GIVEN("Null string")
-    {
-        WHEN("Range-based calculation is performed")
-        {
-            AND_WHEN("Beginning edge is null")
-            {
-                GIVEN("Non-Empty ending string")
-                {
-                    std::string str{"abc"};
-                    auto result = calculateRangeChecksum(nullptr,
-                                                         reinterpret_cast<const unsigned char *>(
-                                                                 str.c_str()));
-
-                    THEN("Result is zero")
-                    {
-                        REQUIRE(result == 0);
-                    }
-                }
-                GIVEN("Empty ending string")
-                {
-                    std::string str;
-                    auto result = calculateRangeChecksum(nullptr,
-                                                         reinterpret_cast<const unsigned char *>(
-                                                                 str.c_str()));
-
-                    THEN("Result is zero")
-                    {
-                        REQUIRE(result == 0);
-                    }
-                }
-            }
-            AND_WHEN("Ending edge is null")
-            {
-                GIVEN("Non-Empty beginning string")
-                {
-                    std::string str{"abc"};
-                    auto result = calculateRangeChecksum(reinterpret_cast<const unsigned char *>(str.c_str()),
-                                                         nullptr);
-
-                    THEN("Result is not zero")
-                    {
-                        REQUIRE(result != 0);
-                    }
-                }
-                GIVEN("Empty beginning string")
-                {
-                    std::string str;
-                    auto result = calculateRangeChecksum(reinterpret_cast<const unsigned char *>(str.c_str()),
-                                                         nullptr);
-
-                    THEN("Result is zero")
-                    {
-                        REQUIRE(result == 0);
-                    }
-                }
-            }
-            AND_WHEN("Both edges are null")
-            {
-                auto result = calculateRangeChecksum(nullptr, nullptr);
-
-                THEN("Result is zero")
-                {
-                    REQUIRE(result == 0);
                 }
             }
         }
